53fbcb306c9286ea04f88f85a6988a697f9f46a5
colinxu666 <colinxu666@gmail.com>
2026-03-14 16:08:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ let jellyy = 100;
 
 document.addEventListener('keydown', function(event) {
   if (event.key === 'a') {
-    jellyx -= 10;
+    jellyx -= 100;
   } else if (event.key === 'd') {
-    jellyx += 10;
+    jellyx += 100;
   } else if (event.key === 'w') {
-    jellyy -= 10;
+    jellyy -= 100;
   } else if (event.key === 's') {
-    jellyy += 10;
+    jellyy += 100;
   }
   jelly.style.left = jellyx + 'px';
   jelly.style.top = jellyy +'px';
