e291422b8e68488ba976819e5a5a7c5c6dcbb04f
colinxu666 <colinxu666@gmail.com>
2026-03-14 17:25:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,46 +8,28 @@ let jelly = {
   image: jellyImg
 }
 
-document.addEventListener('keydown', function(event) {
-  keys[event.key] = true;
-});
-
-document.addEventListener('keyup', function(event) {
-  keys[event.key] = false;
-});
-
 function update() {
-
-  // Horizontal movement
-  if (keys['a']) {
-    jelly.velocityX -= 0.5;
+  if (event.key === 'a') {
+    jelly.velocityX -= 5;
   }
-  if (keys['d']) {
-    jelly.velocityX += 0.5;
+  if (event.key === 'w') {
+    jelly.velocityY -= 10;
   }
-
-  // Jump
-  if (keys['w'] && jelly.velocityY === 0) {
-    jelly.velocityY = -10;
+  if (event.key === 'd') {
+    jelly.velocityX += 5;
   }
-
-  // Gravity
+  //Gravity
   jelly.velocityY += gravity;
   jelly.y += jelly.velocityY;
-
   if (jelly.y > 400) {
     jelly.y = 400;
     jelly.velocityY = 0;
   }
-
-  // Friction
-  jelly.velocityX *= 0.95;
+  //Horizontal Movement
+  jelly.velocityX = jelly.velocityX*0.95;
   jelly.x += jelly.velocityX;
-
-  // Draw
   jelly.image.style.left = jelly.x + 'px';
   jelly.image.style.top = jelly.y + 'px';
-
   requestAnimationFrame(update);
 }
-update()
+update();
