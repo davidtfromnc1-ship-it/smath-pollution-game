a5d2e7304d1db0b0426810710dd3412ffb2b9e20
colinxu666 <colinxu666@gmail.com>
2026-03-14 17:29:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const jellyImg = document.getElementById('jellyfish');
 const gravity = 0.5;
+const jump = 1;
 let jelly = {
   x: 100,
   y: 100,
@@ -23,6 +24,7 @@ document.addEventListener('keydown', function(event) {
 document.addEventListener('keydown', function(event) {
 if (event.key === 'w') {
     jelly.velocityY -= 10;
+    jump = 0;
   }
 });
 
@@ -33,6 +35,9 @@ function update() {
   if (jelly.y > 400) {
     jelly.y = 400;
     jelly.velocityY = 0;
+    if(jump=0){
+      jump=1
+    }
   }
   //Horizontal Movement
   jelly.velocityX = jelly.velocityX*0.95;
