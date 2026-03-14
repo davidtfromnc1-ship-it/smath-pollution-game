68a165970d07bd1f77035343c449d14520fec1bf
colinxu666 <colinxu666@gmail.com>
2026-03-14 17:33:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -34,8 +34,8 @@ function update() {
   jelly.y += jelly.velocityY;
   if (jelly.y > 400) {
     jelly.y = 400;
-    jelly.velocityY === 0;
-    if(jump=0){
+    jelly.velocityY = 0;
+    if(jump === 0){
       jump=1;
     }
   }
