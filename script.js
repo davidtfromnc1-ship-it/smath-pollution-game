8e6a2c76696e0df5fad261339dc8099ab1ffcf1b
davidtfromnc1-ship-it <davidtfromnc1@gmailcom>
2026-03-14 13:55:50 -0400

    update

--- script.js
+++ script.js
@@ -1,15 +1,70 @@
-let pollution = 5;
+let score = 0
+let health = 20
 
-function clean(trash) {
-  trash.remove();
-  pollution -= 1;
-  document.getElementById('pollution').innerText = pollution;
+function spawnTrash(){
 
-  if (pollution <= 0) {
-    alert("🌟 Great job! The ocean is clean!");
-  }
+let trash = document.createElement("img")
+
+trash.src = "assets/trash.png"
+
+trash.className = "trash"
+
+trash.style.left = Math.random()*750 + "px"
+trash.style.top = Math.random()*350 + "px"
+
+trash.onclick = function(){
+
+trash.remove()
+
+score++
+health += 2
+
+document.getElementById("score").innerText =
+"Trash Removed: " + score
+
+updateHealth()
+
+spawnFish()
+
 }
 
-function goToLore() {
-  window.location.href = 'lore.html';
+document.getElementById("ocean").appendChild(trash)
+
+}
+function updateHealth(){
+
+if(health > 100){
+health = 100
+}
+
+document.getElementById("healthBar").style.width =
+health + "%"
+
+}
+function spawnFish(){
+
+if(health > 50){
+
+let fish = document.createElement("img")
+
+fish.src = "assets/fish.png"
+
+fish.style.width = "60px"
+fish.style.position = "absolute"
+
+fish.style.left = Math.random()*750 + "px"
+fish.style.top = Math.random()*350 + "px"
+
+document.getElementById("ocean").appendChild(fish)
+
+}
+
+}
+setInterval(function(){
+
+spawnTrash()
+
+},2000)
+if(score == 10){
+alert("Lore Unlocked: The Plastic Tide - In 2050 there may be more plastic than fish.")
 }
