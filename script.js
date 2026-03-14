72512a69789b1904589f4de9e8b57f7f895a42e2
colinxu666 <colinxu666@gmail.com>
2026-03-14 15:50:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const box = document.getElementById('box');
+let boxX = 100;
+let boxY = 100;
 
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'a') {
+    boxX -= 10;
+  } else if (event.key === 'd') {
+    boxX += 10;
+  } else if (event.key === 'w') {
+    boxY -= 10;
+  } else if (event.key === 's') {
+    boxY += 10;
+  }
+  box.style.left = boxX + 'px';
+  box.style.top = boxY +'px';
+});
