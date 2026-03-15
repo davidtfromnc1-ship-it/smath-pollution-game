bec46ebc248cd03837fd91647658c3dd674ffe9a
colinxu666 <colinxu666@gmail.com>
2026-03-15 14:18:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -68,8 +68,8 @@ let keys = { a: false, d: false, w: false };
 
 const platforms = [
   { x: 0, y: 0, width: 100, height: 1400 },
-  { x: 1200, y: 0, width: 100, height: 300 },
-  { x: 1200, y: 300, width: 100, height: 1000 },
+  { x: 1200, y: 0, width: 100, height: 250 },
+  { x: 1200, y: 450, width: 100, height: 1000 },
   { x: 0, y: 600, width: 800, height: 100 },
   { x: 1000, y: 600, width: 400, height: 100 },
   { x: 1, y: 0, width: 1200, height: 100 },
